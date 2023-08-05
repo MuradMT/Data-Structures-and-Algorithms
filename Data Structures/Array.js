143935c9d5da0a07a5cfaa92e21d91ac1fe0a0cb
Murad Mammadzade <mammadzade03@gmail.com>
2023-08-05 12:00:37 +0400

    done array implementation in ts

---
+++ Data Structures/Array.js
@@ -0,0 +1,44 @@
+"use strict";
+var MyArr = /** @class */ (function () {
+    function MyArr() {
+        this.length = 0;
+        this.data = {};
+    }
+    MyArr.prototype.get = function (index) {
+        return this.data[index];
+    };
+    //Js push function returns length of array
+    MyArr.prototype.push = function (item) {
+        this.data[this.length] = item;
+        this.length++;
+        return this.length;
+    };
+    MyArr.prototype.pop = function () {
+        var item = this.data[this.length - 1];
+        delete this.data[this.length - 1];
+        this.length--;
+        return item;
+    };
+    MyArr.prototype.delete = function (index) {
+        var item = this.data[index];
+        this.shiftItems(index);
+        return item;
+    };
+    MyArr.prototype.shiftItems = function (index) {
+        for (var i = index; i < this.length - 1; i++) {
+            this.data[i] = this.data[i + 1];
+        }
+        delete this.data[this.length - 1];
+        this.length--;
+    };
+    return MyArr;
+}());
+var ar = new MyArr();
+ar.push(12);
+ar.push(89);
+ar.push(90);
+ar.push(56);
+ar.pop();
+ar.delete(2);
+console.log(ar);
+console.log(ar.get(0));
---
+++ Data Structures/Array.ts
@@ -0,0 +1,50 @@
+"use strict";
+class MyArr {
+  length: number;
+  data: any;
+  public constructor() {
+    this.length = 0;
+    this.data = {};
+  }
+  public get(index: number): any {
+    return this.data[index];
+  }
+  //Js push function returns length of array
+  public push(item: any): number {
+    this.data[this.length] = item;
+    this.length++;
+    return this.length;
+  }
+  public repeatDel():void{
+    delete this.data[this.length - 1];
+    this.length--;
+  }
+  public pop(): any {
+    const item = this.data[this.length - 1];
+    this.repeatDel();
+    return item;
+  }
+  public delete(index:number):any{
+    const item=this.data[index];
+    this.shiftItems(index);
+    return item;
+  }
+  public shiftItems(index:number):void{
+    for(let i=index;i<this.length-1;i++){
+        this.data[i]=this.data[i+1];
+    }
+    this.repeatDel();
+  }
+}
+const ar = new MyArr();
+ar.push(12);
+ar.push(89);
+ar.push(90);
+ar.push(56);
+ar.pop();
+ar.delete(2);
+console.log(ar);
+console.log(ar.get(0));
+
+
+
