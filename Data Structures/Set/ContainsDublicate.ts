c495d2dfb5b64c8bb3a27bf0c4ce1b17bf766d15
Murad Mammadzade <mammadzade03@gmail.com>
2023-08-17 12:41:31 +0400

    What is hashtable

--- Data Structures/Set/ContainsDublicate.ts
+++ Data Structures/Set/ContainsDublicate.ts
@@ -2,3 +2,4 @@ function containsDublicate(arr:number[]):boolean{
      return new Set(arr).size!=arr.length;
 }
 console.log(containsDublicate([1,2,3,4,5,1]));
+//it is a type of hashtable only keeps unique keys
