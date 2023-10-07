3b0fcd4dc8972035e6745c585d842f898e084399
Murad Mammadzade <mammadzade03@gmail.com>
2023-10-07 16:32:19 +0400

    solved north east king problem with dynamic programming

---
+++ Algorithms/Dynamic Programming/NorthEastKingDP.py
@@ -0,0 +1,14 @@
+MAX = 1001
+MOD = 1000003
+
+dp = [[0] * MAX for _ in range(MAX)]
+dp[0][0] = 1
+
+for i in range(1, MAX):
+    for j in range(1, MAX):
+        dp[i][j] = (dp[i - 1][j] + dp[i][j - 1] + dp[i - 1][j - 1]) % MOD
+
+tests = int(input().strip())
+for _ in range(tests):
+    n = int(input().strip())
+    print(dp[n][n])
