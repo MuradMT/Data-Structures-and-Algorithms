f871be2d068daae53b3c7829a1972b15f1b4c9dc
Murad Mammadzade <mammadzade03@gmail.com>
2023-12-02 17:08:56 +0400

    Modified Solution

--- Algorithms/SearchingA/Slow_Fast_Two_Pointer.py
+++ Algorithms/SearchingA/Slow_Fast_Two_Pointer.py
@@ -1,20 +1,23 @@
-#move Zeroes problem solved with slow fast approach
 from typing import List
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
+        n=len(nums)
+        if n<2: return
         left,right=0,1
-        while right<len(nums):
-            if nums[left]==0 and nums[right]!=0:
+        while right<n:
+            if nums[left]!=0:
+                left+=1
+                right+=1
+            elif nums[right]==0:
+                right+=1    
+            else:
                 temp=nums[left]
                 nums[left]=nums[right]
                 nums[right]=temp
-                left+=1
-                right+=1
-            elif nums[left]==0 and nums[right]==0:
-                right+=1    
+                
         return nums
 res=Solution()
 print(res.moveZeroes( [0,1,0,3,12]))
